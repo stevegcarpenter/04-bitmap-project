4cfd2bebbe3e6297467dd73bcbc09906298517ff
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-20 13:06:00 -0800

    added code to index.js tha requires 3 commandd line argujents from the user

---
+++ lab-steve-daniel-yohanes-ed/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+  if (process.argv.length !== 5) {
+    console.error('Error: 5 Arguments Are Mandatory');
+    return;
+  }
+
+  console.log('Do Something');
+})();
+
+//input file name .bmp image file
+//transform command
+//output file 'new image name'
