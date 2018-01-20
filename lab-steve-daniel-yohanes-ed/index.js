4f08c7b7176e1759d13c0f374beef786c73c76bb
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-20 14:38:01 -0800

    added a useage function

--- lab-steve-daniel-yohanes-ed/index.js
+++ lab-steve-daniel-yohanes-ed/index.js
@@ -1,13 +1,27 @@
 'use strict';
 
+function usage() {
+  console.log(      
+    `
+    usage:       transform <input-file-path> <output-file-path> <transform-name>
+
+    description: given an input file, output file, and a tranformation
+                 type, this program will modify the original image and 
+                 store the result in the output file.
+    `);
+} 
+
+// Start Execution
 (function () {
   if (process.argv.length !== 5) {
-    console.error('Error: 5 Arguments Are Mandatory');
+    console.error('Error: 3 Arguments Are Mandatory');
+    usage();
     return;
   }
 
   console.log('Do Something');
 })();
+
 
 //input file name .bmp image file
 //transform command
