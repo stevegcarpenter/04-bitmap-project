13c897055781759bc517c47e37c7975c83d422e3
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-20 16:33:29 -0800

    Added a executeProgram function to start the program

--- lab-steve-daniel-yohanes-ed/index.js
+++ lab-steve-daniel-yohanes-ed/index.js
@@ -1,28 +1,56 @@
 'use strict';
 
+const bitmap = require('./lib/bitmap');
+const transform = require('./lib/transform');
+const editor = require('./lib/editor');
+
 function usage() {
-  console.log(      
+  console.log(
     `
     usage:       transform <input-file-path> <output-file-path> <transform-name>
 
     description: given an input file, output file, and a tranformation
-                 type, this program will modify the original image and 
+                 type, this program will modify the original image and
                  store the result in the output file.
-    `);
-} 
+    `
+  );
+}
+
+function executeProgram(options) {
+  let transformFuncs = {
+    greyscale: transform.greyScale,
+    rotate: transform.rotate,
+  };
+
+  // Check if the user entered an invalid tranformation type
+  if (!(options.transformType in transformFuncs)) {
+    console.error(`Error: Invalid transformation ${options.transformType}`);
+    usage();
+    return;
+  }
+
+  options.transformFunc = transformFuncs[options.transformFunc];
+  options.bitMapFunc = bitmap.Buff;
+  options.writeBitMapFunc = editor.writeBitmap;
+  options.testCB = null;
+
+  editor.readBitmap(options.inFilePath, options);
+}
 
 // Start Execution
-(function () {
+(function() {
   if (process.argv.length !== 5) {
     console.error('Error: 3 Arguments Are Mandatory');
     usage();
     return;
   }
 
-  console.log('Do Something');
+  let options = {
+    inFilePath: `${__dirname}/${process.argv[2]}`,
+    outFilePath: `${__dirname}/${process.argv[3]}`,
+    transformType: process.argv[4],
+  };
+
+  // Do it!
+  executeProgram(options);
 })();
-
-
-//input file name .bmp image file
-//transform command
-//output file 'new image name'
