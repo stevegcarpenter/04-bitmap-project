5c3e7c04258c9d47d9e91b7f721808a59dae5cc7
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-01-21 17:38:39 -0800

    finished tests for bitmap constructor, verified non false positives

--- lab-steve-daniel-yohanes-ed/__tests__/bitmap-test.js
+++ lab-steve-daniel-yohanes-ed/__tests__/bitmap-test.js
@@ -13,18 +13,22 @@ describe('Bitmap Module', function () {
     it('Should return a valid instance of Bitmap', function (done) {
       fs.readFile('assets/bitmap.bmp', (err, data) => {
         if (err) console.error(new Error(err));
-        let bmp = Bitmap(data);
-        expect(bmp).toBeInstanceOf(Bitmap);
-        done();
-      }, 25000);
+        if (data) {
+          let bmp = new Bitmap(data);
+          expect(bmp).toBeInstanceOf(Bitmap);
+          done();
+        }
+      });
     });
     it('Should return a valid instance of Bitmap', function (done) {
       fs.readFile('assets/bitmap.bmp', (err, data) => {
         if (err) console.error(new Error(err));
-        let bmp = Bitmap(data);
-        expect(bmp.allData).not.toBeNull();
-        done();
-      }, 25000);
+        if (data) {
+          let bmp = new Bitmap(data);
+          expect(bmp.allData).not.toBeNull();
+          done();
+        }
+      });
     });
   });
 });
